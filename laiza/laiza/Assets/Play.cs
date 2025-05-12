039520ba4c02f6877801d8d35cd3b05cd15db7c9
byliiia <laiza.s@escolar.ifrn.edu.br>
2025-05-12 11:51:16 -0300

    3333

--- laiza/laiza/Assets/Play.cs
+++ laiza/laiza/Assets/Play.cs
@@ -2,15 +2,31 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{ private int numero;
+{
+    private int numero;
     public float velocidade = 40;
+
     void Start()
     {
-    numero = 0;
+        numero = 0;
     }
-    
+
     void Update()
     {
-    //Debyg.Log(numero);
-    //numero * numero * 1
-    
+        //Debyg.Log(numero);
+        //numero * numero * 1;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(velocidade, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(-velocidade, 0, 0);
+        }
+
+
+    }
+
+}
